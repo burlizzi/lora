644c6c0c426f592f5d44ec2979684028d7d27b43
burlizzi <burlizzi@gmail.com>
2024-11-26 06:58:48 +0100

    Update lora.h

--- components/lora/lora.h
+++ components/lora/lora.h
@@ -1,88 +1,61 @@
 #pragma once
-#include "soc/rtc_wdt.h"
+#include "esphome.h"
 #include "esphome/core/controller.h"
 #include "esphome/core/component.h"
 #include "esphome/core/preferences.h"
 #include "esphome/core/automation.h"
-
-//#include <lorawan.h>
-
 #include <RadioLib.h>
 
-void xxx(const char *format, ...);
 
-namespace esphome {
-namespace lora {
-class Lora : public Component, public LoRaWANClass, public Controller {
- public:
-  bool joined;
 
-  Lora();
-  void setup() override;
-  void ook(int data, unsigned char len = 24);
-  void start();
-  void setRX2DR(dataRates_t dr);
-  void reset() {
-    *(uint32_t *) Session_Data.DevAddr = 0;
-    rtc.save(&Session_Data);
-  }
+namespace esphome{
+    namespace lora{
 
-  void send(std::string message = "ciao", unsigned char port = 1, bool confirm = false);
-  void loop() override;
 
-  Trigger<int, int, std::string> *data_trigger() {
-    if (!this->data_trigger_)
-      this->data_trigger_ = make_unique<Trigger<int, int, std::string>>();
-    return this->data_trigger_.get();
-  }
+        class Lora : public Component
+        {
+            public:
+            
 
- protected:
-  std::unique_ptr<Trigger<int, int, std::string>> data_trigger_{nullptr};
-  static const uint32_t RESTORE_STATE_VERSION = 0x2a5a2c13UL;
-  ESPPreferenceObject rtc;
+            Lora();
+            void setDevEUI(uint64_t eui){}
+            void setAppEUI(uint64_t eui){}
+            void setAppKey(std::array<uint8_t,16> eui){}
+            void setDeviceClass(int lora_class){}
 
-#ifdef USE_BINARY_SENSOR
-  void on_binary_sensor_update(binary_sensor::BinarySensor *obj, bool state) override {
-    send(obj->get_object_id_hash(), state);
-  }
-#endif
-#ifdef USE_COVER
-  void on_cover_update(cover::Cover *obj) override;
-#endif
-#ifdef USE_FAN
-  void on_fan_update(fan::FanState *obj) override;
-#endif
-#ifdef USE_LIGHT
-  void on_light_update(light::LightState *obj) override;
-#endif
-#ifdef USE_SENSOR
-  void on_sensor_update(sensor::Sensor *obj, float state) { send(obj->get_object_id_hash(), state); }
-#endif
-#ifdef USE_SWITCH
-  void on_switch_update(switch_::Switch *obj, bool state) override;
-#endif
-#ifdef USE_TEXT_SENSOR
-  void on_text_sensor_update(text_sensor::TextSensor *obj, const std::string &state) override {
-    send(obj->get_object_id_hash(), state);
-  }
-#endif
-#ifdef USE_CLIMATE
-  void on_climate_update(climate::Climate *obj) override;
-#endif
-#ifdef USE_NUMBER
-  void on_number_update(number::Number *obj, float state) override { /*send(obj->get_object_id_hash(),state );*/
-  }
-#endif
-#ifdef USE_SELECT
-  void on_select_update(select::Select *obj, const std::string &state) override {
-    send(obj->get_object_id_hash(), state);
-  }
-#endif
-  template<typename T> inline void send(uint32_t hash, const T &data) {
-    send(hash, std::string((char *) &data, sizeof(data)));
-  }
+            virtual void setup() override
+            {
+                //setup_controller();
+                //uint32_t* dev=(uint32_t*)DevEUI;
+                //rtc = global_preferences->make_preference<sLoRa_Session>(RESTORE_STATE_VERSION^dev[0]^dev[1],true);
+            }
 
-  inline void send(uint32_t hash, const std::string &data) { send(std::string((char *) &hash, sizeof(hash)) + data); }
-};
-}  // namespace lora
-}  // namespace esphome
+           
+
+            void loop() override
+            {
+                char outStr[255];
+                //update();
+                //auto recvStatus = readData(outStr);
+                //if(recvStatus) {
+                //    ESP_LOGI("lora","====>>(%d) rssi(%d) %s",Message_Rx.Frame_Port, getRssi(), outStr);
+                //    if(data_trigger_)
+                //        data_trigger_->trigger(Message_Rx.Frame_Port,getRssi(),std::string(outStr,recvStatus));
+                //}
+            }
+
+            Trigger<int,int,std::string> *data_trigger() {
+                if (!this->data_trigger_)
+                this->data_trigger_ = make_unique<Trigger<int,int,std::string>>();
+                return this->data_trigger_.get();
+            }
+            protected:
+            std::unique_ptr<Trigger<int,int,std::string>> data_trigger_{nullptr};
+            static const uint32_t RESTORE_STATE_VERSION = 0x2a5a2c13UL;
+            ESPPreferenceObject rtc;
+
+        };
+
+
+    }
+}
